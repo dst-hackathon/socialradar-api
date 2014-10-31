349a896333e597f6bfbee3cd230642718134dc10
Prawit Parinyasoontorn <magicboxes99@Prawits-MacBook-Pro.local>
2014-10-31 14:08:34 +0700

    Add database connection

--- question/question.go
+++ question/question.go
@@ -1,6 +1,7 @@
 package question
 
 import (
+	"database/sql"
 	"github.com/gorilla/context"
 	"github.com/gorilla/mux"
 	"gopkg.in/unrolled/render.v1"
@@ -13,6 +14,22 @@ func Init(router *mux.Router) {
 
 func listQuestion(w http.ResponseWriter, req *http.Request) {
 	render := context.Get(req, "render").(*render.Render)
+	db := context.Get(req, "db").(*sql.DB)
 
-	render.JSON(w, http.StatusOK, map[string]string{"hello": "Hello"})
+	rows, err := db.Query("SELECT * FROM questions")
+	if err != nil {
+		render.JSON(w, http.StatusInternalServerError, map[string]string{"error": err.Error()})
+	} else {
+		defer rows.Close()
+
+		var id, text, order string
+		questions := make([]map[string]string, 0)
+		for rows.Next() {
+			rows.Scan(&id, &text, &order)
+			questions = append(questions, map[string]string{"id": id, "text": text, "order": order})
+		}
+
+		render.JSON(w, http.StatusOK, questions)
+	}
+
 }
