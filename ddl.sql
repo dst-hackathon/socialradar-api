f02f6dc6411e50cfc3958f8d66de9af6511c5a41
pongsanti <pongsanti.tanvejsilp@gmail.com>
2014-10-31 13:35:48 +0700

    auto increment id

--- ddl.sql
+++ ddl.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-	id			integer PRIMARY KEY,
+	id			serial PRIMARY KEY,
 	email 		varchar(256) NOT NULL,
 	encrypted_password	varchar(256),
 	created_at 	timestamp DEFAULT current_timestamp,
@@ -10,33 +10,33 @@ CREATE TABLE users (
 );
 
 CREATE TABLE questions (
-	id 		integer PRIMARY KEY,
+	id 		serial PRIMARY KEY,
 	text	varchar(256),
 	display_order	integer DEFAULT 0
 );
 
 CREATE TABLE categories (
-	id		integer PRIMARY KEY,
+	id		serial PRIMARY KEY,
 	text	varchar(256),
 	question_id		integer REFERENCES questions(id),
 	display_order 	integer DEFAULT 0
 );
 
 CREATE TABLE options (
-	id 		integer PRIMARY KEY,
+	id 		serial PRIMARY KEY,
 	text	varchar(256),
 	category_id		integer REFERENCES categories(id),
 	display_order	integer DEFAULT 0
 );
 
 CREATE TABLE users_categories (
-	id 			integer PRIMARY KEY,
+	id 			serial PRIMARY KEY,
 	user_id		integer REFERENCES users(id),
 	category_id integer REFERENCES categories(id)
 );
 
 CREATE TABLE users_options (
-	id 			integer PRIMARY KEY,
+	id 			serial PRIMARY KEY,
 	user_id		integer REFERENCES users(id),
 	option_id	integer REFERENCES options(id)
 );
