d94702ac0001f0beab419b59cfd177af862d665e
pongsanti <pongsanti.tanvejsilp@gmail.com>
2014-11-01 15:45:43 +0700

    put more sample data

--- initial_data.sql
+++ initial_data.sql
@@ -3,14 +3,15 @@ INSERT INTO questions(text, tag, display_order) values ('What sports do you play
 INSERT INTO questions(text, tag, display_order) values ('What are your favorite movies?', 'movie', 3);		-- 3
 INSERT INTO questions(text, tag, display_order) values ('What do music genres belong to you?', 'music', 4); -- 4
 INSERT INTO questions(text, tag, display_order) values ('What are your favorite books?', 'book', 5);		-- 5
+INSERT INTO questions(text, tag, display_order) values ('What type of trip is right for you?', 'travel', 6);-- 6
 
 INSERT INTO categories (text, question_id, display_order) values ('Thai', 1, 1);		-- 1
 INSERT INTO categories (text, question_id, display_order) values ('Japanese', 1, 2);	-- 2
 INSERT INTO categories (text, question_id, display_order) values ('Chinese', 1, 3);	 	-- 3
 INSERT INTO categories (text, question_id, display_order) values ('Western', 1, 4);		-- 4
 
-INSERT INTO categories (text, question_id, display_order) values ('Team', 2, 1);		-- 5
-INSERT INTO categories (text, question_id, display_order) values ('Individual', 2, 2); 	-- 6
+INSERT INTO categories (text, question_id, display_order) values ('Extreme', 2, 1);		-- 5
+INSERT INTO categories (text, question_id, display_order) values ('Motor', 2, 2); 		-- 6
 
 INSERT INTO categories (text, question_id, display_order) values ('Scifi/Fantasy', 3, 1); 	-- 7
 INSERT INTO categories (text, question_id, display_order) values ('Action', 3, 2); 			-- 8
@@ -27,6 +28,15 @@ INSERT INTO categories (text, question_id, display_order) values ('Romance', 5, 
 INSERT INTO categories (text, question_id, display_order) values ('Fantasy', 5, 3); 		-- 17
 INSERT INTO categories (text, question_id, display_order) values ('Fiction', 5, 4); 		-- 18
 
+INSERT INTO categories (text, question_id, display_order) values ('Team', 2, 3); 			-- 19
+INSERT INTO categories (text, question_id, display_order) values ('Individual', 2, 4); 		-- 20
+
+INSERT INTO categories (text, question_id, display_order) values ('Snow and ski', 6, 1); 			-- 21
+INSERT INTO categories (text, question_id, display_order) values ('Cruises', 6, 2); 				-- 22
+INSERT INTO categories (text, question_id, display_order) values ('Activity and adventure', 6, 3); 	-- 23
+
+-- OPTIONS --
+
 INSERT INTO options (text, category_id, display_order) values ('Pad Thai', 1, 1);		-- 1
 INSERT INTO options (text, category_id, display_order) values ('Tom Yum Kung', 1, 2);	-- 2
 INSERT INTO options (text, category_id, display_order) values ('Kao Pad', 1, 3);		-- 3
@@ -37,9 +47,9 @@ INSERT INTO options (text, category_id, display_order) values ('T-bone steak', 4
 INSERT INTO options (text, category_id, display_order) values ('Fish & chip', 4, 2);	-- 8
 INSERT INTO options (text, category_id, display_order) values ('Irish stew', 4, 3);	 	-- 9
 
-INSERT INTO options (text, category_id, display_order) values ('Football', 5, 1);		-- 10
-INSERT INTO options (text, category_id, display_order) values ('Basketball', 5, 2);		-- 11
-INSERT INTO options (text, category_id, display_order) values ('Bowling', 5, 3);		-- 12
+INSERT INTO options (text, category_id, display_order) values ('Caving', 5, 1);			-- 10
+INSERT INTO options (text, category_id, display_order) values ('Kitesurfing', 5, 2);	-- 11
+INSERT INTO options (text, category_id, display_order) values ('Motocross', 5, 3);		-- 12
 
 INSERT INTO options (text, category_id, display_order) values ('Star wars', 7, 1);				-- 13
 INSERT INTO options (text, category_id, display_order) values ('Harry Potter', 7, 2);			-- 14
@@ -79,6 +89,30 @@ INSERT INTO options (text, category_id, display_order) values ('We could be hero
 INSERT INTO options (text, category_id, display_order) values ('Prince Lestat', 18, 1);												-- 46
 INSERT INTO options (text, category_id, display_order) values ('Amnesia', 18, 2);													-- 
 INSERT INTO options (text, category_id, display_order) values ('Horror stories edited', 18, 3);										-- 
+
+INSERT INTO options (text, category_id, display_order) values ('Auto racing', 6, 1);			-- 49
+INSERT INTO options (text, category_id, display_order) values ('Motor rallying', 6, 2);			-- 50
+INSERT INTO options (text, category_id, display_order) values ('Hovercraft racing', 6, 3);		-- 51
+
+INSERT INTO options (text, category_id, display_order) values ('Football', 19, 1);			-- 52
+INSERT INTO options (text, category_id, display_order) values ('Basketball', 19, 2);		-- 
+INSERT INTO options (text, category_id, display_order) values ('Field hockey', 19, 3);		-- 
+
+INSERT INTO options (text, category_id, display_order) values ('Golf', 20, 1);				-- 55
+INSERT INTO options (text, category_id, display_order) values ('Boxing', 20, 2);			-- 
+INSERT INTO options (text, category_id, display_order) values ('Fishing', 20, 3);			-- 
+
+INSERT INTO options (text, category_id, display_order) values ('Zermatt', 21, 1);			-- 58
+INSERT INTO options (text, category_id, display_order) values ('Jackson Hole', 21, 2);		-- 
+INSERT INTO options (text, category_id, display_order) values ('Meribel', 21, 3);			-- 
+
+INSERT INTO options (text, category_id, display_order) values ('Douro', 22, 1);			-- 61
+INSERT INTO options (text, category_id, display_order) values ('Rhine', 22, 2);			-- 
+INSERT INTO options (text, category_id, display_order) values ('Nile', 22, 3);			-- 
+
+INSERT INTO options (text, category_id, display_order) values ('El Salvador', 23, 1);	-- 64
+INSERT INTO options (text, category_id, display_order) values ('Namibia', 23, 2);		-- 
+INSERT INTO options (text, category_id, display_order) values ('Cinque Terre', 23, 3);	-- 
 
 INSERT INTO users(email) values ('pongsanti.tanvejsilp@gmail.com');
 INSERT INTO users(email) values ('prawit@gmail.com');
